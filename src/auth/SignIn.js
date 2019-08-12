45de09745833bb73f604d4ba420ce37c09f02b1a
rayswilling <swillingray@gmail.com>
2019-08-12 14:29:41 +0100

    SignUp Page in Progress

--- src/auth/SignIn.js
+++ src/auth/SignIn.js
@@ -3,6 +3,13 @@ import React from 'react';
 import useSignUpForm from '../CustomHooks';
 
 const Signup = () => {
+  // The form is finally ready! It does everything a normal form should be able to do. It will wait untill all
+  // the required inputs are filled by the user. Then when the user clicks on the button, it will trigger the submit event.
+  // Also when the user types something in an input field, it will store that value in a state variable.
+  // But you as the user are not able to see anything happening in the browser when the button is clicked.
+  // If you remember, we had passed the callback function as a parameter to the handleSubmit function.
+  // So I can simply define this callback function in the Signup.js file like this:
+
   const signup = () => {
     alert(`User Created!
            Name: ${inputs.firstName} ${inputs.lastName}
